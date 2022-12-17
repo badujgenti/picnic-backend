21099e95292c12f9dbaca856d8242e8dfc59de83
badujgenti <badu.jgenti@gmail.com>
2022-12-17 14:11:01 +0400

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import csvParser from "csv-parser";
 import fs from "fs";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.get("/", (req, res) => {
